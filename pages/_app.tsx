8301eab096989236c589cfdc8b51bcecf39d01cf
robertotics4 <robertooliveiragp@gmail.com>
2022-07-08 18:11:24 -0300

    config theme chakra

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import { ChakraProvider } from '@chakra-ui/react';
+
+import { theme } from '../styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider resetCSS theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
 export default MyApp;
